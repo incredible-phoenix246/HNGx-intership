07d5c1994656de70b98e951cb4a68240fc88c8c5
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-07 22:50:35 +0100

    checking

--- Frontend/Task 1/script.js
+++ Frontend/Task 1/script.js
@@ -1,5 +1,29 @@
-// Simulated real-time data update
-setInterval(() => {
-    document.getElementById('dayOfWeek').innerText = 'Tuesday';
-    document.getElementById('utcTime').innerText = '1:30 PM';
-}, 5000); // Updates data every 5 seconds
+function getCurrentDayOfWeek() {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDate = new Date();
+    const dayOfWeek = currentDate.getDay();
+    return daysOfWeek[dayOfWeek];
+}
+
+
+function getCurrentTime() {
+    const currentDate = new Date();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const amOrPm = hours >= 12 ? 'PM' : 'AM';
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${formattedHours}:${formattedMinutes} ${amOrPm}`;
+}
+
+
+function updateData() {
+    const dayOfWeekElement = document.getElementById('dayOfWeek');
+    const timeElement = document.getElementById('utcTime');
+    
+    dayOfWeekElement.innerText = getCurrentDayOfWeek();
+    timeElement.innerText = getCurrentTime();
+}
+
+
+setInterval(updateData, 100);
