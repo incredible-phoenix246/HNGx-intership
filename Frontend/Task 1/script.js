bab207afbb892d45091052596b86a4ca99c8ef50
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-07 22:39:18 +0100

    checking

---
+++ Frontend/Task 1/script.js
@@ -0,0 +1,5 @@
+// Simulated real-time data update
+setInterval(() => {
+    document.getElementById('dayOfWeek').innerText = 'Tuesday';
+    document.getElementById('utcTime').innerText = '1:30 PM';
+}, 5000); // Updates data every 5 seconds
